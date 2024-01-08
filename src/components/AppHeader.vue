9062244f9588cf8f4173e7cb2c96f0134b2e8342
Gabriele Di Rocco <gabriele.dirocco000@gmail.com>
2024-01-08 15:48:49 +0100

    app header

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,18 @@
+<script>
+export default {
+  name: "AppHeader",
+  props: ["message"],
+};
+</script>
+
+<template>
+  <header class="container">
+    <h1 class="text-center my-5">{{ message }}</h1>
+  </header>
+</template>
+
+<style lang="scss" scoped>
+h1 {
+  font-weight: 700;
+}
+</style>
